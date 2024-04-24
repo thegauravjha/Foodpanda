1bbdb8569196a1af833ceda218fca32d8512fdf8
thegauravjha <43226457+thegauravjha@users.noreply.github.com>
2024-04-24 12:30:32 +0530

    changed cross proxy to "https://thingproxy-760k.onrender.com/fetch/"

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const FETCH_MAIN_URL = 'https://corsproxy.io/?'+'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4956341&lng=77.4010397&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+export const FETCH_MAIN_URL = 'https://thingproxy-760k.onrender.com/fetch/'+'https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4956341&lng=77.4010397&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
-export const FETCH_MENU_URL = 'https://corsproxy.io/?'+'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.61450&lng=77.30630&restaurantId=';
+export const FETCH_MENU_URL = 'https://thingproxy-760k.onrender.com/fetch/'+'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.61450&lng=77.30630&restaurantId=';
 
 export const cropText = (text, textLength) => {
     if (typeof (text) === "string") {
